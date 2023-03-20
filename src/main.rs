7df1bb285a9488f8a231a40c909f21e198b3f5ad
kirjavascript <snkenjoi@gmail.com>
2023-03-20 20:59:29 +0000

    test

---
+++ src/main.rs
@@ -0,0 +1,74 @@
+#![no_std]
+#![feature(start)]
+#![allow(unused_imports)]
+
+use core::panic::PanicInfo;
+
+// gym nest ascii 4block
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    // TODO: panic handler
+    let mut p = 0xE0 as *mut u8;
+    for ch in "panic".chars() {
+        unsafe {
+            *p = ch as u8;
+            p = p.add(1);
+        }
+    }
+    loop {}
+}
+
+static PPUSTATUS: usize = 0x2002;
+static PPUCTRL: usize = 0x2000;
+
+fn wait_for_vblank() {
+    let status = PPUSTATUS as *const u8;
+    loop {
+        if unsafe { *status } & 0x80 != 0 { break; }
+    }
+}
+
+#[start]
+fn _main(_argc: isize, _argv: *const *const u8) -> isize {
+    let mut p = 0x100 as *mut u8;
+    for ch in "ram write test".chars() {
+        unsafe {
+            *p = ch as u8;
+            p = p.add(1);
+        }
+    }
+    let p2 = 0xF1 as *mut u8;
+        unsafe { *p2 = 1; };
+    let ctrl = PPUCTRL as *mut u8;
+    unsafe {
+        *ctrl = 0x80;
+    }
+        unsafe { *p2 = 2; };
+    loop {
+
+        unsafe { *p2 = 3; };
+        wait_for_vblank();
+        unsafe { *p2 = 4; };
+        unsafe {
+            *p = '#' as u8;
+            p = p.add(1);
+        }
+        unsafe { *p2 = 5; };
+    }
+    0
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn nmi() -> ! {
+    let p = 0xF0 as *mut u8;
+    unsafe { *p += 1; };
+}
+
+#[link_section = ".nmi.bork"]
+#[no_mangle]
+pub static NMI_VECTOR: unsafe extern "C" fn() -> ! = nmi;
+
+// #[link_section = ".chr_rom"]
+// #[no_mangle]
+// pub static POOP: &[u8] = &[ 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, ];
