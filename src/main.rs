5f57a44976fc4c09df4b6d88aec9dadd35a6e68c
kirjavascript <snkenjoi@gmail.com>
2023-03-22 00:43:07 +0000

    hello world

--- src/main.rs
+++ src/main.rs
@@ -24,16 +24,6 @@ fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
-static PPUSTATUS: usize = 0x2002;
-static PPUCTRL: usize = 0x2000;
-
-fn wait_for_vblank() {
-    let status = PPUSTATUS as *const u8;
-    loop {
-        if unsafe { *status } & 0x80 != 0 { break; }
-    }
-}
-
 #[start]
 fn _main(_argc: isize, _argv: *const *const u8) -> isize {
     let mut p = 0x100 as *mut u8;
@@ -44,8 +34,9 @@ fn _main(_argc: isize, _argv: *const *const u8) -> isize {
         }
     }
 
-    let ctrl = PPUCTRL as *mut u8;
-    unsafe { *ctrl = 0x80; }
+    ppu_ctrl(0x80);
+    ppu_mask(0x1E);
+
     loop {
 
         unsafe { wait_vblank(); }
@@ -53,27 +44,42 @@ fn _main(_argc: isize, _argv: *const *const u8) -> isize {
             *p += 1;
         }
     }
-    0
 }
 
-fn ppu_addr(high: u8, low: u8) {
+fn ppu_ctrl(value: u8) {
+    let p = 0x2000 as *mut u8;
+    unsafe {
+        core::ptr::write_volatile(p, value);
+    }
+}
+
+fn ppu_mask(value: u8) {
+    let p = 0x2001 as *mut u8;
+    unsafe {
+        core::ptr::write_volatile(p, value);
+    }
+}
+
+fn ppu_addr(value: u16) {
     let p = 0x2006 as *mut u8;
     unsafe {
-        *p = high;
-        *p = low;
+        core::ptr::write_volatile(p, (value >> 8) as u8);
+        core::ptr::write_volatile(p, value as u8);
     }
 }
 
 fn ppu_data(value: u8) {
     let p = 0x2007 as *mut u8;
-    unsafe { *p = value; }
+    unsafe {
+        core::ptr::write_volatile(p, value);
+    }
 }
 
 fn ppu_scroll(x: u8, y: u8) {
     let p = 0x2005 as *mut u8;
     unsafe {
-        *p = x;
-        *p = y;
+        core::ptr::write_volatile(p, x);
+        core::ptr::write_volatile(p, y);
     }
 }
 
@@ -83,11 +89,25 @@ pub unsafe extern "C" fn render()  {
     let p = 0xF0 as *mut u8;
     unsafe { *p += 1; };
 
+    ppu_addr(0x2008);
 
-    ppu_addr(0x20, 0x05);
-    ppu_data(5);
-    ppu_data(6);
-    // ppu_scroll(0, 0);
+    for i in 0x2000..0x2040 {
+        ppu_data(0xFF);
+
+    }
+
+    for ch in "HELLO WORLD".chars() {
+        ppu_data(ch as u8 - 65 + 10);
+    }
+
+    for i in 0..10 {
+        ppu_data(0xFF);
+
+    }
+
+
+    ppu_addr(0x2000); // reset scroll
+    ppu_scroll(0, 0);
 }
 
 
